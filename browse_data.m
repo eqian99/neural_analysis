b4e13ef0d5c0121fcab7e6a0d04b7b266f3e9514
Emma Qian <qianhanmin99@gmail.com>
2019-01-18 23:57:07 -0800

    filter

---
+++ browse_data.m
@@ -0,0 +1,5 @@
+
+datafile = '20080516_R1.mat'; % data file name
+load([pwd,filesep,'Data',filesep,datafile]); % load data
+i=1; % recording number
+load([pwd,filesep,'stimulus.mat']); %re-generated stimulus for recording 1
